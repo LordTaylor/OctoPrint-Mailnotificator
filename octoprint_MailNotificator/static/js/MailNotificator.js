a9ddadfdf6189c00192c59b7e5436f60f574bc00
LordTaylor <krawczyk.jaroslaw84@gmail.com>
2020-08-23 21:09:19 +0100

    octoprint init

---
+++ octoprint_MailNotificator/static/js/MailNotificator.js
@@ -0,0 +1,29 @@
+/*
+ * View model for OctoPrint-Mailnotificator
+ *
+ * Author: Jaroslaw Dominik Krawczyk
+ * License: AGPLv3
+ */
+$(function() {
+    function MailnotificatorViewModel(parameters) {
+        var self = this;
+
+        // assign the injected parameters, e.g.:
+        // self.loginStateViewModel = parameters[0];
+        // self.settingsViewModel = parameters[1];
+
+        // TODO: Implement your plugin's view model here.
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: MailnotificatorViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
+        // Elements to bind to, e.g. #settings_plugin_MailNotificator, #tab_plugin_MailNotificator, ...
+        elements: [ /* ... */ ]
+    });
+});
