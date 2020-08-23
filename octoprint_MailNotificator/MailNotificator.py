7e158e898879376003e5bc41e76071a1985e5ce2
LordTaylor <krawczyk.jaroslaw84@gmail.com>
2020-08-23 22:48:32 +0200

    smal ui changes

--- octoprint_MailNotificator/MailNotificator.py
+++ octoprint_MailNotificator/MailNotificator.py
@@ -6,11 +6,21 @@ import MailSender
 class MailNotification(octoprint.plugin.StartupPlugin,
                        octoprint.plugin.TemplatePlugin,
                        octoprint.plugin.SettingsPlugin):
+
+    def get_settings_defaults(self):
+        return [
+                dict(url="eg: smtp.gmail.com"),
+                dict(urlBB="eg: 465")
+               ]
+
     def on_after_startup(self):
         self._logger.info("Yow")
 
     def get_template_vars(self):
-        return dict(url=self._settings.get(["url"]))
+        return [
+            dict(url=self._settings.get(["smtp Mail server"])),
+            dict(urlBB=self._settings.get(["server Port"]))
+            ]
 
 __plugin_name__ = "MailNotification"
 __plugin_version__ = "1.0.0"
