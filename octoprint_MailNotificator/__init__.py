8a55ce37933f9f7db5d8999a9302226d53de0d1d
LordTaylor <krawczyk.jaroslaw84@gmail.com>
2020-08-24 17:20:49 +0200

    ui Test

--- octoprint_MailNotificator/__init__.py
+++ octoprint_MailNotificator/__init__.py
@@ -10,10 +10,17 @@ from __future__ import absolute_import
 # Take a look at the documentation on what other plugin mixins are available.
 
 import octoprint.plugin
+import MailSender
 
 class MailnotificatorPlugin(octoprint.plugin.SettingsPlugin,
                             octoprint.plugin.AssetPlugin,
-                            octoprint.plugin.TemplatePlugin):
+                            octoprint.plugin.TemplatePlugin,
+							octoprint.plugin.ProgressPlugin):
+
+
+	def on_print_progress(storage, path, progress):
+    		MailSender()
+    		
 	def on_after_startup(self):
         	self._settings.get(["url"])
 
@@ -60,7 +67,7 @@ class MailnotificatorPlugin(octoprint.plugin.SettingsPlugin,
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "Mailnotificator"
+__plugin_name__ = "Mail Notificator"
 
 # Starting with OctoPrint 1.4.0 OctoPrint will also support to run under Python 3 in addition to the deprecated
 # Python 2. New plugins should make sure to run under both versions for now. Uncomment one of the following
