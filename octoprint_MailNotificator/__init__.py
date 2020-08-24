51f42953b20ea2272b60dfdc42b056664a0efb27
LordTaylor <krawczyk.jaroslaw84@gmail.com>
2020-08-24 16:45:02 +0200

    ui tests

--- octoprint_MailNotificator/__init__.py
+++ octoprint_MailNotificator/__init__.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-### (Don't forget to remove me)
+# (Don't forget to remove me)
 # This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
 # as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
 # defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
@@ -11,21 +11,24 @@ from __future__ import absolute_import
 
 import octoprint.plugin
 
+
 class MailnotificatorPlugin(octoprint.plugin.SettingsPlugin,
                             octoprint.plugin.AssetPlugin,
                             octoprint.plugin.TemplatePlugin):
 
-	##~~ SettingsPlugin mixin
+	def on_after_startup(self):
+        	self._settings.get(["url"])
+
+
+	# ~~ SettingsPlugin mixin
 
 	def get_settings_defaults(self):
-		return dict(
-			url="eg: smtp.gmail.com"
-		)
+		return dict(url="eg: smtp.gmail.com")
 
 	def get_template_vars(self):
             return dict(url=self._settings.get(["smtp Mail server","server Port"])
 
-	##~~ AssetPlugin mixin
+	# ~~ AssetPlugin mixin
 
 	def get_assets(self):
 		# Define your plugin's asset files to automatically include in the
@@ -36,7 +39,7 @@ class MailnotificatorPlugin(octoprint.plugin.SettingsPlugin,
 			less=["less/MailNotificator.less"]
 		)
 
-	##~~ Softwareupdate hook
+	# ~~ Softwareupdate hook
 
 	def get_update_information(self):
 		# Define the configuration for your plugin to use with the Software Update
@@ -67,9 +70,9 @@ __plugin_name__ = "MailNotificator"
 # Starting with OctoPrint 1.4.0 OctoPrint will also support to run under Python 3 in addition to the deprecated
 # Python 2. New plugins should make sure to run under both versions for now. Uncomment one of the following
 # compatibility flags according to what Python versions your plugin supports!
-#__plugin_pythoncompat__ = ">=2.7,<3" # only python 2
-#__plugin_pythoncompat__ = ">=3,<4" # only python 3
-#__plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
+# __plugin_pythoncompat__ = ">=2.7,<3" # only python 2
+# __plugin_pythoncompat__ = ">=3,<4" # only python 3
+# __plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
 
 def __plugin_load__():
 	global __plugin_implementation__
