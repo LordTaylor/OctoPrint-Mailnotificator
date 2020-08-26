7d63942a2deab6ce9e99ffc17ba900e246a7ab81
LordTaylor <krawczyk.jaroslaw84@gmail.com>
2020-08-26 23:08:18 +0200

    ghjk

--- MailNotificator/__init__.py
+++ MailNotificator/__init__.py
@@ -35,17 +35,17 @@ class MailnotificatorPlugin(octoprint.plugin.SettingsPlugin,
 			less=["less/MailNotificator.less"]
 		)
 	
-	def get_template_configs(self):
-			return [
-				dict(type="navbar", custom_bindings=False),
-				dict(type="settings", custom_bindings=False)
-			]
-	
-	# def get_template_vars(self):
+	# def get_template_configs(self):
 	# 		return [
 	# 			dict(type="navbar", custom_bindings=False),
 	# 			dict(type="settings", custom_bindings=False)
 	# 		]
+	
+	def get_template_vars(self):
+			return [
+				dict(type="navbar", custom_bindings=False),
+				dict(type="settings", custom_bindings=False)
+			]
 
 	##~~ Softwareupdate hook
 
