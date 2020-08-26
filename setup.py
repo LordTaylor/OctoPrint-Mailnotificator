79abd4f52e1f24925b4df41caa1cd58323328635
LordTaylor <krawczyk.jaroslaw84@gmail.com>
2020-08-26 23:30:10 +0200

    nnbv

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "Mailnotificator"
+plugin_identifier = "mailnotificator"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
 plugin_package = "mailnotificator"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "Mailnotificator"
+plugin_name = "mailnotificator"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "0.1.0"
