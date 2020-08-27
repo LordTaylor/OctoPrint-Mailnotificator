80e938716f6351c507f08b7c2aed2865a522bd99
LordTaylor <krawczyk.jaroslaw84@gmail.com>
2020-08-27 20:26:49 +0100

    dgsdf

--- mailnotificator/static/js/MailNotificator.js
+++ mailnotificator/static/js/MailNotificator.js
@@ -4,7 +4,7 @@
  * Author: Jaroslaw Dominik Krawczyk
  * License: AGPLv3
  */
-$(function () {
+$(function() {
     function MailnotificatorViewModel(parameters) {
         var self = this;
 
@@ -18,7 +18,7 @@ $(function () {
 
         // this will be called when the user clicks the "Go" button and set the iframe's URL to
         // the entered URL
-        self.goToUrl = function () {
+        self.goToUrl = function() {
             self.currentUrl(self.newUrl());
         };
 
@@ -26,8 +26,8 @@ $(function () {
         // dependencies have already been initialized. It is especially guaranteed that this method
         // gets called _after_ the settings have been retrieved from the OctoPrint backend and thus
         // the SettingsViewModel been properly populated.
-        self.onBeforeBinding = function () {
-            self.newUrl(self.settings.settings.plugins.mailnotificator.url());
+        self.onBeforeBinding = function() {
+            self.newUrl(self.settings.settings.plugins.MailNotificator.url());
             self.goToUrl();
         }
     }
@@ -36,11 +36,11 @@ $(function () {
      * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
      * and a full list of the available options.
      */
-    OCTOPRINT_VIEWMODELS.push([
-        MailnotificatorViewModel,
+    OCTOPRINT_VIEWMODELS.push({
+        construct: MailnotificatorViewModel,
         // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
-        [ /* "loginStateViewModel",*/ "settingsViewModel"],
+        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
         // Elements to bind to, e.g. #settings_plugin_MailNotificator, #tab_plugin_MailNotificator, ...
-        ["#tab_plugin_MailNotificator", "#settings_plugin_MailNotificator"]
-    ]);
+        elements: [ /* ... */ ]
+    });
 });
